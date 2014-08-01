cf3a8fe78d9ced75e284fd14c0190a94aa9c2d3a
Joseph Hsu <jhsu.x1@gmail.com>
2014-07-31 19:10:40 -0700

    add more grades and style color buttons

--- app/models/grade.js
+++ app/models/grade.js
@@ -13,6 +13,13 @@ Grade.reopenClass({
     {id: 5, name: 'V3'},
     {id: 6, name: 'V4'},
     {id: 7, name: 'V5'},
+    {id: 8, name: 'V6'},
+    {id: 9, name: 'V7'},
+    {id: 10, name: 'V8'},
+    {id: 11, name: 'V9'},
+    {id: 12, name: 'V10'},
+    {id: 13, name: 'V11'},
+    {id: 14, name: 'V12'},
   ]
 });
 
