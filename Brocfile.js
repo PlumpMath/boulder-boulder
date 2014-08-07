bc6e0a18ba81df47c96291df46c3fe4ceb2c937b
Joseph Hsu <jhsu.x1@gmail.com>
2014-08-07 11:25:22 -0700

    foundation js import for different environments

--- Brocfile.js
+++ Brocfile.js
@@ -17,5 +17,9 @@ var app = new EmberApp();
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
 
-app.import('vendor/foundation/js/foundation.js');
+app.import({
+  development: 'vendor/foundation/js/foundation.js',
+  production: 'vendor/foundation/js/foundation.min.js'
+});
+
 module.exports = app.toTree();
